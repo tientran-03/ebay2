4ccff4f792e7bb2ea5f736b21694b59ec2459e52
Administrator <admin@ebay-app.local>
2025-11-15 12:57:41 +0700

    Add logging configuration and logs directory

--- ebay/30_TranVietTien/30_TranVietTien/Program.cs
+++ ebay/30_TranVietTien/30_TranVietTien/Program.cs
@@ -9,6 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+// Add logging configuration
+builder.Host.ConfigureLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.AddConsole();
+    logging.AddDebug();
+    logging.SetMinimumLevel(LogLevel.Information);
+});
+
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(60);
